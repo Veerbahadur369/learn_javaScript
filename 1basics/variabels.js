10a7edc7e2b57741afed656e5b1a88ddb4d18e63
Veerbahadur369 <bveee369@gmail.com>
2025-04-14 03:13:57 +0000

    container config done

---
+++ 1basics/variabels.js
@@ -0,0 +1,11 @@
+const emailId="veer@gmail.com";
+let password=23456;
+var city="buxar"
+
+// emailId="veerB@gmail.com";
+password=76543;
+city="faridabad";
+let state="Haryana"
+state="Bihar"
+console.table([city,emailId,password,state]);
+console.log(password);
