ed2e74baee721f98478da1271c9fe36ec321e700
Veerbahadur369 <bveee369@gmail.com>
2025-04-15 05:39:23 +0000

    done

---
+++ 1basics/2_converstion_operation.js
@@ -0,0 +1,16 @@
+// let num=34
+// let val=Number(num);
+// console.log(typeof(val))
+// console.log(val)
+// let bool=Boolean(num);
+// // console.log(bool);
+// let st=String(num);
+// console.log(4+54+"object")
+
+// let num1=new Date()
+// console.log(num1.toDateString())
+// let val1=Number(num1)
+// console.log(val1)
+// console.log(typeof Number(val1))
+
+// +++++++++++++++++  Operations  ++++++++++++
